813df3593fb9c69f3c392d3aa3aef06700764618
Minju <angertiger0941@gmail.com>
2025-03-16 23:19:54 +0900

    사다리 타기

--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -1,8 +1,39 @@
 public class Ladder {
-
     private final int[][] rows;
+    private final int numberOfPerson;
+    private final int row;
 
     public Ladder(int row, int numberOfPerson) {
         rows = new int[row][numberOfPerson];
+        this.numberOfPerson = numberOfPerson;
+        this.row = row;
+    }
+
+    public void drawLine(int line, int level) {
+        if (line < 1 || line > numberOfPerson - 1) {
+            throw new IllegalArgumentException("선택된 라인과 그 오른편의 라인을 연결하므로 가장 끝 라인을 제외한 라인을 선택하세요.");
+        }
+        if (level < 1 || level > row) {
+            throw new IllegalArgumentException("가능한 높이를 벗어났습니다.");
+        }
+        if (rows[level-1][line - 1] != 0 || rows[level-1][line] != 0) {
+            throw new IllegalArgumentException("이미 선점된 위치입니다.");
+        }
+        rows[level-1][line - 1] = 1;
+        rows[level-1][line] = -1;
+    }
+
+    public int run(int selectedLine) {
+        if (selectedLine < 1 || selectedLine > numberOfPerson) {
+            throw new IllegalArgumentException("선택할 수 없는 번호입니다.");
+        }
+
+        int currentLine = selectedLine - 1;
+
+        for (int level = 0; level < row; level++) {
+            currentLine += rows[level][currentLine];
+        }
+
+        return currentLine + 1;
     }
 }
--- src/test/java/LadderTest.java
+++ src/test/java/LadderTest.java
@@ -1,5 +1,79 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LadderTest {
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 8, 12})
+    @DisplayName("사다리 그리기 - 잘못된 라인 선택")
+    void testDrawLineWhenSelectedWrongLine(int selectedLine){
+        int givenRow = 8, givenNumberOfPerson = 5;
+        Ladder ladder = new Ladder(givenRow, givenNumberOfPerson);
 
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> ladder.drawLine(selectedLine, 1));
+        assertEquals("선택된 라인과 그 오른편의 라인을 연결하므로 가장 끝 라인을 제외한 라인을 선택하세요.", exception.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 12, 100})
+    @DisplayName("사다리 그리기 - 잘못된 높이 선택")
+    void testDrawLineWhenSelectedLineIsOutOfRange(int selectedLevel){
+        int givenRow = 8, givenNumberOfPerson = 5;
+        Ladder ladder = new Ladder(givenRow, givenNumberOfPerson);
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> ladder.drawLine(1, selectedLevel));
+        assertEquals("가능한 높이를 벗어났습니다.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("사다리 그리기 - 이미 왼쪽이 선점된 선택")
+    void testDrawLineWhenSelectedLineIsNotAvailableAtLeft(){
+        int givenRow = 8, givenNumberOfPerson = 5;
+        Ladder ladder = new Ladder(givenRow, givenNumberOfPerson);
+        ladder.drawLine(1, 1);
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> ladder.drawLine(2, 1));
+        assertEquals("이미 선점된 위치입니다.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("사다리 그리기 - 이미 오른쪽이 선점된 선택")
+    void testDrawLineWhenSelectedLineIsNotAvailableAtRight(){
+        int givenRow = 8, givenNumberOfPerson = 5;
+        Ladder ladder = new Ladder(givenRow, givenNumberOfPerson);
+        ladder.drawLine(3, 1);
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> ladder.drawLine(2, 1));
+        assertEquals("이미 선점된 위치입니다.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("사다리 타기")
+    void testRun(){
+        int givenRow = 8, givenNumberOfPerson = 5;
+        Ladder ladder = new Ladder(givenRow, givenNumberOfPerson);
+        ladder.drawLine(1,1);
+        ladder.drawLine(4,1);
+        ladder.drawLine(2,2);
+        ladder.drawLine(4,2);
+        ladder.drawLine(3,3);
+        ladder.drawLine(2,4);
+        ladder.drawLine(4,4);
+        ladder.drawLine(1,5);
+        ladder.drawLine(4,6);
+        ladder.drawLine(3,7);
+        ladder.drawLine(1,7);
+        ladder.drawLine(1,8);
+        ladder.drawLine(3,8);
+
+        assertEquals(4, ladder.run(1));
+        assertEquals(2, ladder.run(2));
+        assertEquals(3, ladder.run(3));
+        assertEquals(1, ladder.run(4));
+        assertEquals(5, ladder.run(5));
+    }
 }
