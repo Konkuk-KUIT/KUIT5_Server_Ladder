2ecc1684946cd63eb9d0549929d0077c1d22d8cd
yeonjy73 <yeon703@naver.com>
2025-03-18 00:44:18 +0900

    test: 사다리 타기 기본 테스트 구현

--- src/test/java/LadderTest.java
+++ src/test/java/LadderTest.java
@@ -1,5 +1,41 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
 
 class LadderTest {
+
+    @ParameterizedTest
+    @DisplayName("사다리에 Line 이 없을 때 시작 번호 그대로 반환해야한다.")
+    @ValueSource(ints = {1, 2, 3})
+    void testRunWithoutDrawLine(int targetNumber){
+        //given
+        Ladder ladder = new Ladder(2, 3);
+
+        //when
+        int result = ladder.run(targetNumber);
+
+        //then
+        assertThat(result).isEqualTo(targetNumber);
+    }
+
+    @Test
+    @DisplayName("line 이 있을 때, run 메소드 동작 테스트")
+    void testRunWithDrawLines(){
+        //given
+        Ladder ladder = new Ladder(3, 3);
+
+        //when
+        ladder.drawLine(1, 2, 1);
+        ladder.drawLine(2, 3, 2);
+        ladder.drawLine(3, 2, 3);
+
+        int result = ladder.run(1);
+
+        //then
+        assertThat(result).isEqualTo(2);
+    }
 
 }
