403d08fd1faf52bfdfd0ee64e6bb55dc2839987b
KelseyKwon <nykwon7777@gmail.com>
2025-03-17 10:09:27 +0900

    feat: 초기완성

--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -5,4 +5,51 @@ public class Ladder {
     public Ladder(int row, int numberOfPerson) {
         rows = new int[row][numberOfPerson];
     }
+
+    public void drawLine(int start1, int end1, int start2, int end2) {
+        if (rows[start1][end1] == 0) {
+            rows[start1][end1] = 1;
+            rows[start2][end2] = -1;
+        }
+    }
+    //run : 몇 번째 사다리를 선택하면 도착하는 사다리의 번호를 알 수 있도록 구현하기.
+    //
+
+    public int run(int numOfPerson) {
+        int selectedPerson = numOfPerson - 1;
+
+        for (int i = 0; i < rows.length; i++) {
+            if (rows[i][selectedPerson] == 0) {
+                continue;
+            }
+//            if (rows[i][selectedPerson] == 1) {
+//                selectedPerson += 1;
+//            }
+//            if (rows[i][selectedPerson] == -1) {
+//                selectedPerson -= 1;
+//            }
+            selectedPerson = move(i, selectedPerson);
+        }
+
+        return selectedPerson + 1;
+    }
+
+    private int move(int row, int selectedPerson) {
+        if (rows[row][selectedPerson] == 1) {
+            return selectedPerson += 1;
+        }
+        if (rows[row][selectedPerson] == -1) {
+            return selectedPerson -= 1;
+        }
+        return selectedPerson;
+    }
+
+    public void printLadder() {
+        for (int i = 0; i < rows.length; i++) {
+            for (int j = 0; j < rows[i].length; j++) {
+                System.out.print(rows[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
--- src/test/java/LadderTest.java
+++ src/test/java/LadderTest.java
@@ -1,5 +1,102 @@
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LadderTest {
+
+    @Test
+    @DisplayName("사다리 모양 출력 - 같은 거 중복 안되는지 확인")
+    void PrintLadder() {
+        //given
+        Ladder ladder = new Ladder(3, 3);
+        ladder.drawLine(0, 0, 0, 1);
+        ladder.drawLine(0, 1, 0, 2);
+        ladder.drawLine(1, 1, 1, 2);
+        ladder.drawLine(2, 0, 2, 1);
+
+        //when&then
+        ladder.printLadder();
+    }
+
+    @Test
+    @DisplayName("사람이 1명일때")
+    void OnePeople() {
+        //given
+        Ladder ladder = new Ladder(2, 1);
+
+        //when
+        int selectedPerson = ladder.run(1);
+
+        //then
+        assertThat(selectedPerson).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("사람이 2명 이상, 짝수일때")
+    void moreThanTwoPeopleEven() {
+        //given
+        Ladder ladder = new Ladder(2, 2);
+        ladder.drawLine(0, 0, 0, 1);
+        ladder.drawLine(1, 0, 1, 1);
+
+        //when
+        int selectedPerson = ladder.run(1);
+
+        //then
+        assertThat(selectedPerson).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("사람이 2명 이상, 짝수일때")
+    void moreThanTwoPeopleEven2() {
+        //given
+        Ladder ladder = new Ladder(3, 4);
+        ladder.drawLine(0, 0, 0, 1);
+        ladder.drawLine(1, 1, 1, 2);
+        ladder.drawLine(2, 2, 2, 3);
+
+        //when
+        int selectedPerson = ladder.run(1);
+
+        //then
+        assertThat(selectedPerson).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("사람이 2명 이상, 홀수일때")
+    void moreThanTwoPeopleOdd() {
+        //given
+        Ladder ladder = new Ladder(3, 3);
+        ladder.drawLine(0, 0, 0, 1);
+        ladder.drawLine(1, 1, 1, 2);
+        ladder.drawLine(2, 0, 2, 1);
+
+        //when
+        int selectedPerson = ladder.run(3);
+
+        //then
+        assertThat(selectedPerson).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("같은 행에 2개 이상이 있을때")
+    void moreThanOneLineInARow() {
+        //given
+        Ladder ladder = new Ladder(3, 3);
+        ladder.drawLine(0, 0, 0, 1);
+        ladder.drawLine(0, 1, 0, 2);
+        ladder.drawLine(1, 1, 1, 2);
+        ladder.drawLine(2, 0, 2, 1);
+
+        //when
+        int selectedPerson = ladder.run(3);
+
+        //then
+        assertThat(selectedPerson).isEqualTo(1);
+    }
+
 
 }
