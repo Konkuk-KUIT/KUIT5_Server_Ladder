26dd483b2cff7339bb4a85f897f4b17e2ff8d1ef
yeonjy73 <yeon703@naver.com>
2025-03-18 00:43:43 +0900

    feat: 사다리 타기 구현

--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -5,4 +5,32 @@ public class Ladder {
     public Ladder(int row, int numberOfPerson) {
         rows = new int[row][numberOfPerson];
     }
+
+    public void drawLine(int start, int end, int row) {
+        if (start < end){
+            rows[row - 1][start - 1] = 1;
+            rows[row - 1][end - 1] = -1;
+            return;
+        }
+        if (start > end){
+            rows[row - 1][start - 1] = -1;
+            rows[row - 1][end - 1] = 1;
+        }
+    }
+
+    public int run(int targetNumber){
+        int targetColumn = targetNumber - 1;
+
+        for (int[] row : rows) {
+            if (row[targetColumn] == 1) {
+                targetColumn++;
+                continue;
+            }
+            if (row[targetColumn] == -1) {
+                targetColumn--;
+            }
+        }
+        return targetColumn + 1;
+    }
+
 }
