e1aefcaf1c44c4cfe521245ac6dd903984c6a4a6
KelseyKwon <nykwon7777@gmail.com>
2025-03-20 14:27:50 +0900

    feat: 배열 범위 검사 메소드 추가

--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -6,15 +6,22 @@ public class Ladder {
         rows = new int[row][numberOfPerson];
     }
 
+    /**
+     * 유효한 좌표인지 확인 후,
+     * 시작 점과 끝 점이 모두 0 이여야 사다리 줄이 그어진다.
+     */
     public void drawLine(int start1, int end1, int start2, int end2) {
-        if (rows[start1][end1] == 0) {
+        validatePosition(start1, end1, start2, end2);
+        if (checkIfItIsZero(start1, end1, start2, end2))
+        {
             rows[start1][end1] = 1;
             rows[start2][end2] = -1;
         }
     }
-    //run : 몇 번째 사다리를 선택하면 도착하는 사다리의 번호를 알 수 있도록 구현하기.
-    //
 
+    /**
+     * 몇 번째 사다리를 선택하면 도착하는 사다리의 번호를 알 수 있도록 구현하기.
+     */
     public int run(int numOfPerson) {
         int selectedPerson = numOfPerson - 1;
 
@@ -22,16 +29,24 @@ public class Ladder {
             if (rows[i][selectedPerson] == 0) {
                 continue;
             }
-//            if (rows[i][selectedPerson] == 1) {
-//                selectedPerson += 1;
-//            }
-//            if (rows[i][selectedPerson] == -1) {
-//                selectedPerson -= 1;
-//            }
             selectedPerson = move(i, selectedPerson);
         }
 
         return selectedPerson + 1;
+    }
+
+    private boolean checkIfItIsZero(int start1, int end1, int start2, int end2) {
+        return rows[start1][end1] == 0 && rows[start2][end2] == 0;
+    }
+
+    private void validatePosition(int start1, int end1, int start2, int end2) {
+        if (start1 < 0 || start1 >= rows.length
+                || start2 < 0 || start2 >= rows.length
+                || end1 < 0 || end1 >= rows[0].length
+                || end2 < 0 || end2 >= rows[0].length) {
+            throw new IllegalArgumentException("유효하지 않은 숫자입니다 : [start1 : " + start1 + ", end1 : " + end1 +
+                    ", start2" + start2 + ", end2 : " + end2 + "\n 배열 위치에서 벗어나지 않은 숫자를 입력해주세요");
+        }
     }
 
     private int move(int row, int selectedPerson) {
--- src/test/java/LadderTest.java
+++ src/test/java/LadderTest.java
@@ -62,7 +62,7 @@ class LadderTest {
         int selectedPerson = ladder.run(1);
 
         //then
-        assertThat(selectedPerson).isEqualTo(3);
+        assertThat(selectedPerson).isEqualTo(4);
     }
 
     @Test
@@ -85,17 +85,48 @@ class LadderTest {
     @DisplayName("같은 행에 2개 이상이 있을때")
     void moreThanOneLineInARow() {
         //given
-        Ladder ladder = new Ladder(3, 3);
+        Ladder ladder = new Ladder(3, 4);
         ladder.drawLine(0, 0, 0, 1);
+
+        ladder.drawLine(1, 1, 1, 2);
+        //아래 두개는 안그어짐.
+        ladder.drawLine(1, 0, 1, 1);
+        ladder.drawLine(1, 2, 1, 3);
+
+        ladder.drawLine(2, 1, 2, 2);
+
+        //when
+        int selectedPerson = ladder.run(1);
+
+        //then
+        assertThat(selectedPerson).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("유효하지 않은 숫자가 넘어올 떄")
+    void notAValidNumber() {
+        //given
+        Ladder ladder = new Ladder(3, 3);
+        // 음수
+//        ladder.drawLine(-1, 0, 0, 1);
+        assertThrows(IllegalArgumentException.class, () -> ladder.drawLine(-1, 0, 0, 1));
+
+        // 범위 벗어난 것.
+//        ladder.drawLine(3, 1, 0, 2);
+//        ladder.drawLine(1, 3, 1, 2);
+        assertThrows(IllegalArgumentException.class, () -> ladder.drawLine(3, 1, 0, 2));
+        assertThrows(IllegalArgumentException.class, () -> ladder.drawLine(1, 3, 1, 2));
+        assertThrows(IllegalArgumentException.class, () -> ladder.drawLine(1, 0, 3, 2));
+        assertThrows(IllegalArgumentException.class, () -> ladder.drawLine(1, 0, 0, 3));
+
+
+        //나머지는 정상
         ladder.drawLine(0, 1, 0, 2);
         ladder.drawLine(1, 1, 1, 2);
         ladder.drawLine(2, 0, 2, 1);
 
-        //when
-        int selectedPerson = ladder.run(3);
-
-        //then
-        assertThat(selectedPerson).isEqualTo(1);
+        //when&then
+        ladder.printLadder();
     }
 
 
