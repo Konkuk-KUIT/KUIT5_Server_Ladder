5a30b1c5c4b91e448dc22d8829c9d1fd96d888c8
dasol <pds2023@gmail.com>
2025-03-21 22:56:30 +0900

    refactor: direction 표현을 enum으로 변경

--- src/main/java/Direction.java
+++ src/main/java/Direction.java
@@ -1,3 +1,12 @@
-public class Direction {
+public enum Direction {
+    LEFT(-1), RIGHT(1), NONE(0);
+    private final int value;
 
+    Direction(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
