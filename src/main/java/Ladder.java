e9e5b2ff1bac840e8851730a5624ec95727bab0d
Chae-Yu <chaeyu2004@naver.com>
2025-03-21 14:02:17 +0900

    refactor: Ladder 클래스와 Line 클래스 중복 로직 분리
    - 줄 그리는 로직 책임 분리

--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -11,26 +11,31 @@ public class Ladder {
     }
 
     public void drawLine(int row, int col) {
-        if (row < 0 || row >= rows.length || col < 0 || col >= rows[0].length -1) {
+        if (row < 0 || row >= rows.length || col < 0 || col >= rows[0].length - 1) {
             throw new IllegalArgumentException("잘못된 사다리 위치입니다.");
         }
 
-        if (rows[row][col] != 0 || rows[row][col+1] != 0){
+        if (rows[row][col] != 0 || rows[row][col + 1] != 0){
             throw new IllegalArgumentException("이미 선이 그어져 있거나 연속된 선입니다.");
         }
 
+        Line line = new Line(rows[row]);
+        line.validateDrawLine(col);
+
+        // 선 그리기
         rows[row][col] = 1;
         rows[row][col+1] = -1;
     }
 
     public int run (int startPosition){
-        int position  = startPosition;
+        Position position = new Position(startPosition);
 
-        for (int row = 0; row < rows.length; row++){
-            int direction = rows[row][position];
-            position += direction;
+        for (int row = 0; row < rows.length; row++) {
+            int direction = rows[row][position.getIndex()];
+            position.move(direction);
         }
-        return position;
+
+        return position.getIndex();
     }
 
 }
--- src/main/java/Line.java
+++ src/main/java/Line.java
@@ -2,25 +2,16 @@ public class Line {
     // 사다리의 한 줄마다 이동하는 로직
     private final int[] moves;
 
-    public Line(int width) {
-        this.moves = new int[width];
+    public Line(int[] moves) {
+        this.moves = moves;
     }
 
-    public void draw(int col) {
+    public void validateDrawLine(int col) {
         if (col >= moves.length - 1) {
             throw new IllegalArgumentException("마지막 열에는 선을 그을 수 없습니다.");
         }
         if (moves[col] != 0 || moves[col + 1] != 0) {
             throw new IllegalStateException("연속된 선이거나 이미 선이 그어져 있습니다.");
         }
-        moves[col] = 1;
-        moves[col + 1] = -1;
     }
-
-    public void move(Position position) {
-        int direction = moves[position.getIndex()];
-        position.move(direction);
-    }
-
-
 }
