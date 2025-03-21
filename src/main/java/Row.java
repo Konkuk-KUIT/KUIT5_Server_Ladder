8dbb2d8c1a3c7803b2399857256ce18f1971f43e
Minju <angertiger0941@gmail.com>
2025-03-22 00:15:29 +0900

    refacor: test class 나누기

--- src/main/java/Row.java
+++ src/main/java/Row.java
@@ -14,7 +14,7 @@ public class Row {
         }
     }
     private final List<Integer> row;
-
+    // NaturalNumber 객체 사용하자: 양수 들어올거 expect 되면
     private Row(List<Integer> row) {
         this.row = row;
     }
@@ -22,12 +22,12 @@ public class Row {
     public static Row create(int size) {
         return new Row(new ArrayList<>(Collections.nCopies(size, 0)));
     }
-
     public void drawLine(int index) {
         validate(index);
         row.set(index - 1, Direction.MOVE_RIGHT.value);
         row.set(index, Direction.MOVE_LEFT.value);
     }
+
 
     private void validate(int index) {
         if (index < 1 || index > row.size() - 1) {
@@ -37,6 +37,9 @@ public class Row {
             throw new IllegalArgumentException(ExceptionMessage.ALREADY_EXIST_POSITION.getMessage());
         }
     }
+    // 이름이 별로야. 바로 메소드의 의미를 파악하기가 어렵다 ㅠㅠ. 뭐를 검사하는 지 추가해주면 좋을 것 같다.
+    // cmd + e : 탭 전환
+    // 찢어.
 
     public int move(int index) {
         return index + row.get(index);
---
+++ src/test/java/LadderRunTest.java
@@ -0,0 +1,51 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class LadderRunTest {
+    @ParameterizedTest
+    @ValueSource(ints = {6, 10})
+    @DisplayName("사다리 타기 - 잘못된 라인 선택")
+    void testRunWhenSelectedLineIsOutOfRange(int selectedLine) {
+        int givenRow = 4, givenNumberOfPerson = 5;
+        Ladder ladder = Ladder.create(givenRow, givenNumberOfPerson);
+        ladder.drawLine(1,1);
+        ladder.drawLine(4,1);
+        ladder.drawLine(2,2);
+        ladder.drawLine(4,2);
+
+        assertThatThrownBy(() -> ladder.run(selectedLine)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionMessage.INVALID_RUN_LINE.getMessage());
+    }
+
+    @Test
+    @DisplayName("사다리 타기 - 정상 작동")
+    void testRun(){
+        int givenRow = 8, givenNumberOfPerson = 5;
+        Ladder ladder = Ladder.create(givenRow, givenNumberOfPerson);
+        ladder.drawLine(1,1);
+        ladder.drawLine(4,1);
+        ladder.drawLine(2,2);
+        ladder.drawLine(4,2);
+        ladder.drawLine(3,3);
+        ladder.drawLine(2,4);
+        ladder.drawLine(4,4);
+        ladder.drawLine(1,5);
+        ladder.drawLine(4,6);
+        ladder.drawLine(3,7);
+        ladder.drawLine(1,7);
+        ladder.drawLine(1,8);
+        ladder.drawLine(3,8);
+
+        assertEquals(4, ladder.run(1));
+        assertEquals(2, ladder.run(2));
+        assertEquals(3, ladder.run(3));
+        assertEquals(1, ladder.run(4));
+        assertEquals(5, ladder.run(5));
+    }
+}
