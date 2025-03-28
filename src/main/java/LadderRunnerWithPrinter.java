0b77bd4d8987d7d8cfb40b102969b889a96f4da4
MinseoSONG <mins041600@naver.com>
2025-03-28 21:46:16 +0900

    [feat] : LadderRunner class 구현

--- src/main/java/LadderRunnerWithPrinter.java
+++ src/main/java/LadderRunnerWithPrinter.java
@@ -1,12 +1,15 @@
 import exception.ExceptionMessage;
 
-public class LadderRunner {
+public class LadderRunnerWithPrinter {
 
-    private LadderRunner() {
+    private final LadderPrinter printer;
+
+    private LadderRunnerWithPrinter(LadderPrinter printer) {
+        this.printer = printer;
     }
 
-    public static LadderRunner from() {
-        return new LadderRunner();
+    public static LadderRunnerWithPrinter from(LadderPrinter printer) {
+        return new LadderRunnerWithPrinter(printer);
     }
 
     public int run(Ladder ladder, NaturalNumber start) {
@@ -17,13 +20,21 @@ public class LadderRunner {
         int[][] rows = ladder.getRows();
         LadderPosition position = LadderPosition.of(0, start.getValue() - 1);
 
+        printer.printWithMarker(position, Time.BEFORE);
+
         while (position.getX() < rows.length) {
             int direction = rows[position.getX()][position.getY()];
             LadderPosition moved = position.move(direction);
-            position = moved.down();
+            printer.printWithMarker(moved, Time.AFTER);
+
+            LadderPosition down = moved.down();
+            if (down.getX() < rows.length) {
+                printer.printWithMarker(down, Time.BEFORE);
+            }
+
+            position = down;
         }
 
         return position.getY() + 1;
     }
 }
-
